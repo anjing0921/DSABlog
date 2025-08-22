673349f89ab103423f688b2819c84ede3b924ad8
anjing0921 <duyuan0921@gmial.com>
2025-08-22 16:46:09 -0700

    add comment section

--- client/src/pages/Blog.jsx
+++ client/src/pages/Blog.jsx
@@ -15,7 +15,7 @@ const Blog = () => {
     }
 
     const fetchComments = async () =>{
-        setComments(data.comments)
+        setComments(comments_data)
 }
 
     useEffect(()=>{
@@ -45,6 +45,22 @@ const Blog = () => {
             </div>
 
             <div className='rich-text max-w-3xl mx-auto' dangerouslySetInnerHTML={{__html: data.description}}></div>
+            {/* Comments Section */}
+            <div className='mt-14 mb-10 max-w-3xl mx-auto'>
+                <p className='font-semibold mb-4'>Comments ({comments.length})</p>
+                <div className='flex flex-col gap-4'>
+                    {comments.map((item, index)=>(
+                    <div key={index} className='relative bg-primary/2 border border-primary/5 max-w-xl p-4 rounded text-gray-600'>
+                        <div className='flex items-center gap-2 mb-2'>
+                        <img src={assets.user_icon} alt="" className='w-6'/>
+                        <p className='font-medium'>{item.name}</p>
+                        </div>
+                        <p className='text-sm max-w-md ml-8'>{item.content}</p>
+                        <div className='absolute right-4 bottom-3 flex items-center gap-2 text-xs'>{Moment(item.createdAt).fromNow()}</div>
+                    </div>
+                    ))}
+                </div>
+            </div>
 
             
         </div>    
