372287ea8574883a3f700d5a765a153ee4de36f4
anjing0921 <duyuan0921@gmial.com>
2025-08-22 16:49:10 -0700

    add add commnet section

--- client/src/pages/Blog.jsx
+++ client/src/pages/Blog.jsx
@@ -61,6 +61,18 @@ const Blog = () => {
                     ))}
                 </div>
             </div>
+            {/* Add Comment Section */}
+            <div className='max-w-3xl mx-auto'>
+                <p className='font-semibold mb-4'>Add your comment</p>
+                <form onSubmit={addComment} className='flex flex-col items-start gap-4 max-w-lg'>
+
+                    <input onChange={(e)=> setName(e.target.value)} value={name} type="text" placeholder='Name' required className='w-full p-2 border border-gray-300 rounded outline-none'/>
+
+                    <textarea onChange={(e)=> setContent(e.target.value)} value={content} placeholder='Comment' className='w-full p-2 border border-gray-300 rounded outline-none h-48' required></textarea>
+
+                    <button type="submit" className='bg-primary text-white rounded p-2 px-8 hover:scale-102 transition-all cursor-pointer'>Submit</button>
+                </form>
+            </div>
 
             
         </div>    
