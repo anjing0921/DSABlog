636510558b4584096933a1692aa27586fdc8b6e0
anjing0921 <duyuan0921@gmial.com>
2025-08-22 15:09:10 -0700

    add footer section

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -3,7 +3,7 @@ import Navbar from '../components/Navbar'
 import Header from '../components/Header'
 import BlogList from '../components/BlogList'
 import Newsletter from '../components/Newsletter'
-// import Footer from '../components/Footer'
+import Footer from '../components/Footer'
 
 const Home = () => {
   return (
@@ -12,7 +12,7 @@ const Home = () => {
       <Header/>
       <BlogList/>
       <Newsletter />
-      {/* <Footer /> */}
+      <Footer/>
 
     </div>
   )
