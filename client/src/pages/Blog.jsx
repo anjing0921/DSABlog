113699a2cb24104fa405ebeeecab867852a99376
anjing0921 <duyuan0921@gmial.com>
2025-08-22 16:32:36 -0700

    add externl link

--- client/src/pages/Blog.jsx
+++ client/src/pages/Blog.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react'
 import {useParams} from 'react-router-dom'
 import { assets, blog_data, comments_data } from '../assets/assets'
 import Navbar from '../components/Navbar'
+import Moment from 'moment'
 
 const Blog = () => {
     const {id} = useParams()
@@ -27,11 +28,24 @@ const Blog = () => {
         <Navbar/>
 
         <div className='text-center mt-20 text-gray-600'>
-            
+            <p className='text-primary py-4 font-medium'>Published on {Moment(data.createdAt).format('MMMM Do YYYY')}</p> 
             <h1 className='text-2xl sm:text-5xl font-semibold max-w-2xl mx-auto text-gray-800'>{data.title}</h1>
             <h2 className='my-5 max-w-lg truncate mx-auto'>{data.subTitle}</h2>
             <p className='inline-block py-1 px-4 rounded-full mb-6 border text-sm border-primary/35 bg-primary/5 font-medium text-primary'>Michael Brown</p>
         </div>
+
+        <div className='mx-5 max-w-5xl md:mx-auto my-10 mt-6'>
+            {/* <img src={data.image} alt="" className='rounded-3xl mb-5'/> */}
+            <div className='grid place-items-center'>
+                <a href={data.external_link} target="_blank">
+                    Click here to visit {data.title} problem
+                </a>
+            </div>
+
+            <div className='rich-text max-w-3xl mx-auto' dangerouslySetInnerHTML={{__html: data.description}}></div>
+
+            
+        </div>    
 
         
         
