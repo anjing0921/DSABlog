b7dc372239f14b8088e80951fe5f4e6cf84ff0e2
anjing0921 <duyuan0921@gmial.com>
2025-08-22 22:49:41 -0700

    add upload thumbnail section

--- client/src/pages/admin/AddBlog.jsx
+++ client/src/pages/admin/AddBlog.jsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import React, { useEffect, useRef, useState } from 'react'
+import { assets, blogCategories } from '../../assets/assets'
+
+
+
 
 const AddBlog = () => {
-  return (
-    <div>AddBlog</div>
-  )
+    
+
+    const [image, setImage] = useState(false);
+    const [title, setTitle] = useState('');
+    const [subTitle, setSubTitle] = useState('');
+    const [category, setCategory] = useState('Startup');
+    const [isPublished, setIsPublished] = useState(false);
+
+    const onSubmitHandler = async (e) =>{
+        e.preventDefault();
+    }
+    
+    return (
+        <form onSubmit={onSubmitHandler} className='flex-1 bg-blue-50/50 text-gray-600 h-full overflow-scroll'>
+            <div className='bg-white w-full max-w-3xl p-4 md:p-10 sm:m-10 shadow rounded'>
+
+                <p>Upload thumbnail</p>
+                <label htmlFor="image">
+                    <img src={!image ? assets.upload_area : URL.createObjectURL(image)} alt="" className='mt-2 h-16 rounded cursor-pointer'/>
+                    <input onChange={(e)=> setImage(e.target.files[0])} type="file" id='image' hidden required/>
+                </label>
+            </div>
+        </form>
+    )
 }
 
 export default AddBlog
