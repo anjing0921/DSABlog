0b484acdeb6ef28bb60b556d2d9dd34bdbdfe6fd
anjing0921 <duyuan0921@gmial.com>
2025-08-23 13:03:26 -0700

    add pushlish button

--- client/src/pages/admin/AddBlog.jsx
+++ client/src/pages/admin/AddBlog.jsx
@@ -62,7 +62,16 @@ const AddBlog = () => {
                         return <option key={index} value={item}>{item}</option>
                     })}
                 </select>
-            </div>
+
+                <div className='flex gap-2 mt-4'>
+                    <p>Publish Now</p>
+                    <input type="checkbox" checked={isPublished} className='scale-125 cursor-pointer' onChange={e => setIsPublished(e.target.checked)}/>
+                </div>
+
+                <button disabled={isAdding} type="submit" className='mt-8 w-40 h-10 bg-primary text-white rounded cursor-pointer text-sm'>
+                    {isAdding ? 'Adding...' : 'Add Blog'}
+                </button>
+                </div>
         </form>
     )
 }
