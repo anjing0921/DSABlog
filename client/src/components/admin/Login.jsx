ac0196d99b5fe0bd9cad0f985268ffb9e12b6a0c
anjing0921 <duyuan0921@gmial.com>
2025-08-22 20:36:26 -0700

    add login page css

--- client/src/components/admin/Login.jsx
+++ client/src/components/admin/Login.jsx
@@ -3,7 +3,12 @@ import React, { useState } from 'react'
 
 const Login = () => {
 
-    
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+
+    const handleSubmit = async (e)=>{
+        e.preventDefault()
+    }
 
     return (
         <div className='flex items-center justify-center h-screen'>
@@ -13,7 +18,7 @@ const Login = () => {
                         <h1 className='text-3xl font-bold'><span className='text-primary'>Admin</span> Login</h1>
                         <p className='font-light'>Enter your credentials to access the admin panel</p>
                     </div>
-                    {/* <form onSubmit={handleSubmit} className='mt-6 w-full sm:max-w-md text-gray-600'>
+                    <form onSubmit={handleSubmit} className='mt-6 w-full sm:max-w-md text-gray-600'>
                         <div className='flex flex-col'>
                             <label> Email </label>
                             <input onChange={e=> setEmail(e.target.value)} value={email} 
@@ -25,7 +30,7 @@ const Login = () => {
                             type="password" required placeholder='your password' className='border-b-2 border-gray-300 p-2 outline-none mb-6'/>
                         </div>
                         <button type="submit" className='w-full py-3 font-medium bg-primary text-white rounded cursor-pointer hover:bg-primary/90 transition-all'> Login </button>
-                    </form> */}
+                    </form>
                 </div>
             </div>
         </div>
