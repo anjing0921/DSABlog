ea3b3b725066ad2e1b8f5fe6facdc0399f85bc13
anjing0921 <duyuan0921@gmial.com>
2025-08-22 20:24:09 -0700

    add side bar

--- client/src/pages/admin/Layout.jsx
+++ client/src/pages/admin/Layout.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { assets } from '../../assets/assets'
 import { Outlet, useNavigate } from 'react-router-dom'
-// import Sidebar from '../../components/admin/Sidebar'
+import Sidebar from '../../components/admin/Sidebar'
 
 
 const Layout = () => {
@@ -18,7 +18,10 @@ const Layout = () => {
                 <img src={assets.logo} alt="" className='w-32 sm:w-40 cursor-pointer' onClick={()=> navigate('/')}/>
                 <button onClick={logout} className='text-sm px-8 py-2 bg-primary text-white rounded-full cursor-pointer'>Logout</button>
             </div>
-            
+            <div className='flex h-[calc(100vh-70px)]'>
+                <Sidebar />
+                
+            </div>
         </>
     )
 }
