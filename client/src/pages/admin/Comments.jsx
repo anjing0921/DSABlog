082970d21b407d259c4e01e5cf000dd8c6509e00
anjing0921 <duyuan0921@gmial.com>
2025-08-22 22:28:02 -0700

    add approved button

--- client/src/pages/admin/Comments.jsx
+++ client/src/pages/admin/Comments.jsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { comments_data } from '../../assets/assets'
+// import CommentTableItem from '../../components/admin/CommentTableItem'
+
 
 const Comments = () => {
-  return (
-    <div>Comments</div>
-  )
+
+    const [comments, setComments] = useState([])
+    const [filter, setFilter] = useState('Not Approved')
+
+    const fetchComments = async ()=>{
+
+        setComments(comments_data) 
+    }
+
+    useEffect(()=>{
+        fetchComments()
+    },[])
+    return (
+        <div className='flex-1 pt-5 px-5 sm:pt-12 sm:pl-16 bg-blue-50/50'>
+            <div className='flex justify-between items-center max-w-3xl'>
+                <h1>Comments</h1>
+                <div className='flex gap-4'>
+                    <button className={`shadow-custom-sm border rounded-full px-4 py-1 cursor-pointer text-xs ${filter === 'Approved' ? 'text-primary' : 'text-gray-700'}`}>Approved</button>
+
+                    
+                </div>
+            </div>
+            
+    </div>
+    )
 }
 
 export default Comments
