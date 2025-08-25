2b47408aa445b95f1a0ac8b4bd7f3fb6cdca3c66
anjing0921 <duyuan0921@gmial.com>
2025-08-25 11:35:06 -0700

    update the search function in the header.jsx

--- client/src/components/admin/Login.jsx
+++ client/src/components/admin/Login.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 import { useAppContext } from '../../context/AppContext'
 import toast from 'react-hot-toast';
-import { data } from 'react-router-dom';
+// import { data } from 'react-router-dom';
 
 
 const Login = () => {
