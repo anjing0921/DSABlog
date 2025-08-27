fd35130290a45219e7669885c80b46d4b3b858de
anjing0921 <duyuan0921@gmial.com>
2025-08-27 13:22:13 -0700

    add generate comtent

--- server/controllers/blogController.js
+++ server/controllers/blogController.js
@@ -1,5 +1,6 @@
 import Blog from "../models/Blog.js";
 import Comment from "../models/Comment.js";
+import main from '../configs/gemini.js';
 
 export const addBlog = async (req, res) => {
   try {
@@ -103,3 +104,13 @@ export const getBlogComments = async (req, res) => {
     res.json({ success: false, message: error.message });
   }
 };
+
+export const generateContent = async (req, res)=>{
+    try {
+        const {prompt} = req.body;
+        const content = await main(prompt + ' Generate a blog content for this topic in simple text format')
+        res.json({success: true, content})
+    } catch (error) {
+        res.json({success: false, message: error.message})
+    }
+}
