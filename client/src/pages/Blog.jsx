d701d94244bee3154533804348390d7f7a3ea1b7
anjing0921 <duyuan0921@gmial.com>
2025-08-28 15:43:20 -0700

    delete the sub title

--- client/src/pages/Blog.jsx
+++ client/src/pages/Blog.jsx
@@ -69,7 +69,6 @@ const Blog = () => {
             <div className='text-center mt-20 text-gray-600'>
                 <p className='text-primary py-4 font-medium'>Published on {Moment(data.createdAt).format('MMMM Do YYYY')}</p> 
                 <h1 className='text-2xl sm:text-5xl font-semibold max-w-2xl mx-auto text-gray-800'>{data.title}</h1>
-                {/* <h2 className='my-5 max-w-lg truncate mx-auto'>{data.subTitle}</h2> */}
             </div>
             <div className='mx-7 max-w-5xl md:mx-auto my-10 mt-6'>
                 <div className='my-4 grid place-items-center'>
